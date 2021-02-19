76190ac8cc33a9815147ccbc7b8825fe24fe0be9
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-02-18 23:17:10 -0600

    update.1.4

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-  const inquirer = require('inquirer');
-  const fs = require('fs');
-  // Epmployee template based on thise below.
-  const Manager = require("./lib/Manager");
-  const Intern = require("./lib/Intern");
-  const Engineer = require("./lib/Engineer");
-  const { type } = require('os');
-  // array fills with employee data
-  const teamMembers = [];
-  // manager will change so hast to be a let
-  let manager;
-  // info is for html
-  let teamTitle;
-  // function manager data
-  function managerData() {
-  inquirer.prompt([
+    const inquirer = require('inquirer');
+    const fs = require('fs');
+    // Epmployee template based on thise below.
+    const Manager = require("./lib/Manager");
+    const Intern = require("./lib/Intern");
+    const Engineer = require("./lib/Engineer");
+    // array fills with employee data
+    const teamMembers = [];
+    // manager will change so must be a let var
+    let manager;
+    // info is for html
+    let teamTitle;
+    // function manager data arrey
+    function managerData() {
+    inquirer.prompt([
     { // fill html with teamName
       type: 'input',
       name: 'teamTitle',
@@ -34,45 +33,40 @@
       type: 'input',
       name: 'number of office',
       message: 'what is the manager office number',
-    }]).then(managerAnswers => {
-        manager = Manager(managerAnswers.managerName, managerAnswers.managerID, managerAnswers.managerEmail, managerAnswers.numberofoffice, managerAnswers.teamTitle);
+    }]).then(managerData => {
+        manager = Manager(managerData.managerName, managerData.managerID, managerData.managerEmail, managerData.numberofoffice, managerData.teamTitle);
         console.log("now ask for employee information")
-        lesserEmployeeData();
-       });
-    // function will repeat if more than one employee is needed   
-    function lesserEmployeeData(){
-    inquirer.prompt([
-     {// employeerole arreay
-      type: 'list',
-      name: 'EmployeeRole',
-      message: 'what is this empolyee role',
-      choices: ['Intern', 'Engineer']
-     },{// questions for employee
-      type: 'input',
-       name: '',
-       message: ''
-     },{
-       type: 'input',
-       name: '',
-       message: ''
-     },{
-      type: 'input',
-      name: '',
-      message: ''
-    },{
-      type: 'input',
-      name: '',
-      message: '',
-      when:
-    },{
-      type: 'input',
-      name: '',
-      message: '',
-      when:
-    },{
-      type: 'input',
-      name: '',
-      message: ''
+        employeeData();
+    });// function will repeat if more than one employee is needed   
+       function employeeData(){
+       inquirer.prompt([
+    { // employeerole arreay
+       type: 'list',
+       name: 'EmployeeRole',
+       message: 'what is this empolyee role',
+       choices: ['Intern', 'Engineer']
+    },{ // questions for employee
+        type: 'input',
+        name: 'employeeName',
+        message: 'what is the employee name'
+    },{ type: 'input',
+        name: 'employeeID',
+        message: 'what is the employee id'
+    },{ type: 'input',
+        name: 'employeeEmail',
+        message: 'what is the employee email'
+    },{ type: 'input',
+        name: 'github',
+        message: 'what is the engineers GitHub',
+        when: (userInput) => userInput.employeeRole === "Engineer"
+    },{ type: 'input',
+        name: 'school',
+        message: 'what is the Interns school',
+        when: ( userInput) => userInput.employeeRole === "Intern"
+    },{ type: 'input',
+        name: 'newEmployee',
+        message: 'would you like to add another team member'
+        // if you choose yes it will go back to the start and if you choose no it will renderHTML
     }
     ])
   }
